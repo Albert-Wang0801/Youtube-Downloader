4894b324677faf5f812b689e0f93bff7509c8643
Albert-Wang0801 <93241302+Albert-Wang0801@users.noreply.github.com>
2022-03-13 11:33:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from pytube import YouTube
+import moviepy.editor 
+import os ,time
+
+while(True):
+    # 要檢查的檔案路徑
+    video_folder = "./video"
+    audio_folder = "./audio"
+
+    # 檢查檔案是否存在
+    if os.path.isfile(video_folder):
+        print("建立資料夾: video")
+    if os.path.isfile(audio_folder):
+        print("建立資料夾: audio")
+
+    #定義下載 Youtube 檔案
+    url = str(input("請貼上 Youtube 影片連結(輸入 EXIT 結束):　"))
+    if(url == "EXIT"):
+        break
+    file = str(input("請輸入名稱:　"))
+
+    #宣告 pytube 函式值
+    yt = YouTube(url)
+
+    print("開始下載影片")
+    yt.streams.get_highest_resolution().download("./video" ,filename = file + ".mp4")
+    print("影片下載完成")
+    time.sleep(0.5)
+    os.system("cls")
+
+    print("開始轉換檔案")
+    video = moviepy.editor.VideoFileClip("./video/" + file + ".mp4")
+    video.audio.write_audiofile("./audio/" + file + ".mp3")
+    print("檔案轉換完成")
+    time.sleep(0.5)
+    os.system("cls")
